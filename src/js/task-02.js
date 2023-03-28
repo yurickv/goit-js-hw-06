bf10b257c9183336e2c831356f41e865d7b54ef0
yura teslyuk <116177992+yurickv@users.noreply.github.com>
2023-03-28 14:58:52 +0300

    complite task 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+let itemList =[];
+
+for (let i = 0; i < ingredients.length; i++) {
+  
+  const arreyHtml = document.createElement('li')
+  arreyHtml.textContent = ingredients[i];
+  arreyHtml.classList.add('item');
+  itemList.push(arreyHtml);
+}
+
+document.querySelector('ul').append(...itemList)
+
+console.log(document.querySelector('ul'))
