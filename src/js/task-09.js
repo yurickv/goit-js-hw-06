618513c2db1b9e787c10415bb15bf15347766bf9
yura teslyuk <116177992+yurickv@users.noreply.github.com>
2023-04-01 01:47:03 +0300

    task9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,20 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const buttonEl = document.querySelector('.change-color')
+const spanEl = document.querySelector('.color')
+const divEl =  document.querySelector('.widget')
+
+
+buttonEl.addEventListener('click', changeColor);
+
+function changeColor() {
+  const RundomeColor = getRandomHexColor();
+  
+  divEl.style.backgroundColor = RundomeColor;
+
+  spanEl.textContent = RundomeColor;
+
+  // console.log(RundomeColor);
+}
