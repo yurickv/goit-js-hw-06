c0470c620fbc5cab935a208bf6c30cc9b53bb9be
yura teslyuk <116177992+yurickv@users.noreply.github.com>
2023-04-01 01:23:54 +0300

    task8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,22 @@
+const formEl = document.querySelector('.login-form')
 
+formEl.addEventListener('submit', sendForm)
+
+function sendForm(event) {
+    event.preventDefault();
+
+    const { email, password } = event.currentTarget.elements;
+    
+    if (email.value === "" || password.value === "") {
+        return alert("Please fill in all the fields!");
+    }
+    
+    const data = {
+        email: email.value,
+        password: password.value,
+    }
+    
+    console.log(data);
+
+    formEl.reset();
+}
